0c615ffcfeab3ac8b3f24ab7c3e2d86760eb2dfb
borispipo <borispipo@gmail.com>
2022-10-16 05:30:42 +0100

    Prise en compte du package inline-dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
 module.exports = function(api,opts) {
     opts = typeof opts =='object' && opts ? opts : {};
+    ///les chemin vers la variable d'environnement, le chemin du fichier .env,@see : https://github.com/brysgo/babel-plugin-inline-dotenv
+    const environmentPath = opts.environmentPath || opts.envPath;
     const path = require("path");
     const dir = path.resolve(__dirname);
     api.cache(true);
@@ -13,6 +15,9 @@ module.exports = function(api,opts) {
         ['babel-plugin-root-import', {"paths": alias}],
         ['react-native-reanimated/plugin'],
         ['transform-inline-environment-variables'],
+        ['inline-dotenv',{
+          ...(environmentPath?{path:environmentPath} : {})
+        }]
       ]
     };
   };

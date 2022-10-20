c786a0498abeedf8927e8e27a4f20e102b4776d1
borispipo <borispipo@gmail.com>
2022-10-20 05:36:31 +0100

    Renommer le script publish en publish1 Fix : The "path" argument must be of type string. Received an instance of Array

--- expo-ui-path.js
+++ expo-ui-path.js
@@ -7,7 +7,12 @@ const path = require("path");
 const dir = path.resolve(__dirname)
 ///retourne le chemin vers le package @expo-ui
 module.exports = function ([...args]){
-    const suffix = path.resolve(args);
+    let suffix = "";
+    args.map(a=>{
+        if(typeof a =='string' && a){
+            suffix+=(suffix?(path.resolve(suffix,a)):a);
+        }
+    });
     console.log("suffix is suffix heinn ",suffix)
     const p = lookupForExpoUIPath();
     if(p && fs.existsSync(p)){

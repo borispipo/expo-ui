8618b481f553eac4b8b6e24756cd0aa183dfeff4
borispipo <borispipo@gmail.com>
2022-11-08 23:27:19 +0100

    Fix common sur datagrid

--- src/components/Datagrid/Common/Common.js
+++ src/components/Datagrid/Common/Common.js
@@ -1531,8 +1531,8 @@ export default class CommonDatagridComponent extends AppComponent {
                  if(!_render) _render = rowData[columnField]
              } else if(arrayValueExists(_type,['switch','checkbox'])){
                  let {checkedLabel,checkedTooltip,uncheckedTooltip,checkedValue,uncheckedLabel,uncheckedValue} = columnDef;
-                 checkedLabel = defaultStr(checkedLabel,checkedTooltip,'Désactivé/Désélectionné')
-                 uncheckedLabel = defaultStr(uncheckedLabel,uncheckedTooltip,'Activé/Sélectionné')
+                 checkedLabel = defaultStr(checkedLabel,checkedTooltip,'Oui')
+                 uncheckedLabel = defaultStr(uncheckedLabel,uncheckedTooltip,'Non')
                  checkedValue = defaultVal(checkedValue,1); uncheckedValue = defaultVal(uncheckedValue,0)
                  let val = defaultVal(rowData[columnField],columnDef.defaultValue,columnDef.value)
                  if(val === checkedValue){

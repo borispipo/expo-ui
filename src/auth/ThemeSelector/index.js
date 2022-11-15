b6df1324f5a092702aba63e505d0b531ea192abc
borispipo <borispipo@gmail.com>
2022-11-15 22:25:41 +0100

    Update du package common

--- src/auth/ThemeSelector/index.js
+++ src/auth/ThemeSelector/index.js
@@ -85,7 +85,7 @@ export const getThemeFieldProps = (props,ref)=>{
                     open((isEditing?"Modification ":"Enregistrement ")+"du thème...");
                     Auth.upsertUser(user,false).then(()=>{
                         if(Auth.getLoggedUserCode() == user.code){
-                            login(user,false);
+                            login(user,true);
                         }
                         if(ref && ref.current && ref.current.refresh){
                             ref.current.refresh(true);

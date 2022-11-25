01fbc00eee9e466261db575cac351de577d15dd0
borispipo <borispipo@gmail.com>
2022-11-25 13:24:22 +0100

    Désactiver le screen NetworkLogin

--- src/screens/mainScreens.js
+++ src/screens/mainScreens.js
@@ -1,8 +1,8 @@
 import Auth from "./Auth";
 import Help  from "./Help";
-import NetworkLoginScreen from "./NetworkLogin";
+//import NetworkLoginScreen from "./NetworkLogin";
 export default  [
     ...Auth,
     ...Help,
-    NetworkLoginScreen,
+    //NetworkLoginScreen,
 ]

793d455d423a90bb90d4f445f8cc8cd73df85136
borispipo <borispipo@gmail.com>
2022-11-27 12:49:53 +0100

    Update package pour fix le bug lié à la compilation android

--- expo-ui-path.js
+++ expo-ui-path.js
@@ -6,26 +6,34 @@ const fs = require("fs");
 const path = require("path");
 const { hideBin } = require('yargs/helpers')
 const yargs = require('yargs/yargs');
+let pathRef = {current:null};
+const metroConfig = "metro.config";
 ///retourne le chemin vers le package @expo-ui
 module.exports = function (...args){
-    const argv = yargs(hideBin(process.argv)).argv
-    let isBuild = false;
-    const expoUIPath = "@fto-consult/expo-ui";
-    if(typeof argv == 'object' && argv && !Array.isArray(argv)){
+    const argv = yargs(hideBin(process.argv)).argv;
+    const suffix = path.join(...args);
+    const isMetroConfig = suffix && suffix.toLowerCase().includes(metroConfig);
+    let isBuild = isMetroConfig ? false : pathRef.current ? true : false;
+    let expoUIPath = isBuild ? pathRef.current : "@fto-consult/expo-ui";
+    if(isMetroConfig && typeof argv == 'object' && argv && !Array.isArray(argv)){
         for(let i in argv){
             let v = argv[i];
             if(typeof v !='string' || !v) continue;
             v = v.toLowerCase(); 
             i = (i+'').toLocaleLowerCase();
-            if(i.includes("export") || v.includes('build') || v.includes('production') || v.includes('export') || v.includes('android')){
-                isBuild = true;
-                break;
+            if(i != '$0'){
+                if(v.includes('production') || v.includes('export') || v.includes('android')){
+                    isBuild = true;
+                    break;
+                }
             }
         }
     }
-    const suffix = path.join(...args);
     const sep = path.sep;
     if(isBuild){
+        if(isMetroConfig){
+            pathRef.current = expoUIPath;
+        }
         const pp = suffix ? path.join(expoUIPath,suffix).replace(sep,"/") : expoUIPath;
         return pp;
     }
@@ -34,6 +42,9 @@ module.exports = function (...args){
         const rootPath = path.resolve(p,"..");
         const src = path.resolve(rootPath,"src");
         if(fs.existsSync(src) && fs.existsSync((path.resolve(rootPath,"babel.config.js")))){
+            if(isMetroConfig){
+                pathRef.current = path.resolve(p);
+            }
             return path.resolve(p,suffix).replace(sep,(sep+sep));
         }
     }

f8124e572840247f5edafdb7ff2c72411b12ee32
borispipo <borispipo@gmail.com>
2023-01-02 17:56:28 +0100

    Afficher à nouveau les indicateurs scrollview dans le datagrid ACCORDION

--- src/components/Datagrid/Common/Common.js
+++ src/components/Datagrid/Common/Common.js
@@ -2798,7 +2798,7 @@ export default class CommonDatagridComponent extends AppComponent {
         return <View testID={testID+"_ContentContainer"}  style={[theme.styles.w100,isA && this.state.displayOnlySectionListHeaders && {borderTopColor:theme.colors.divider,borderTopWidth:1},isA ? [theme.styles.ph2,theme.styles.pt1] : [theme.styles.pt1,theme.styles.ph1],theme.styles.justifyContentCenter,theme.styles.alignItemsCenter,theme.styles.pb1,!cells && theme.styles.ml1,theme.styles.mr1,cStyle]}>
             <Label testID={testID+"_Label"} splitText numberOfLines={3} textBold style={[theme.styles.w100,{color:theme.colors.primaryOnSurface,fontSize:isA?15 :16},lStyle]}>{label}</Label>
             {cells ? <View testID={testID+"_TableRow"} style = {[theme.styles.w100,theme.styles.row,isA && theme.styles.pt1,theme.styles.alignItemsFlexStart]}
-            >{cells}</View> : null}
+            >{this.isAccordion() ? <ScrollView testID={testID+"_ScrollView"} horizontal>{cells}</ScrollView> : cells}</View> : null}
         </View>
     }
     isRowSelected(rowKey,rowIndex){

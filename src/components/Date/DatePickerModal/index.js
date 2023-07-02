4bb8f318635dadcd293d012e1438b269560e9f3e
borispipo <borispipo@gmail.com>
2023-07-02 10:34:22 +0100

    Supprimer le message de debugin

--- src/components/Date/DatePickerModal/index.js
+++ src/components/Date/DatePickerModal/index.js
@@ -4,7 +4,6 @@ import {PaperProvider} from 'react-native-paper';
 import theme from "$theme";
 
 export default function DatePickerModalComponent(props){
-    console.log(theme," is theme heeeee")
     return <PaperProvider theme={theme}>
         <DatePickerModal {...props}/>
     </PaperProvider>

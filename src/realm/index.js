38c916126643fe18d911b35d614af76bd19125a5
borispipo <borispipo@gmail.com>
2023-09-09 07:15:23 +0100

    Supprimer le fichier app.config.$common dans $common

--- src/realm/index.js
+++ src/realm/index.js
@@ -3,3 +3,12 @@ import Realm from "realm";
 export default Realm;
 
 export * from "realm";
+
+export class Model extends Realm.Object{
+    constructor(props){
+        super(props);
+    }
+    static fields;
+    static tableName;//Table name.
+    static name;
+}

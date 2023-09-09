f303d369568dd949148f6cc8a197ffb16e27017e
borispipo <borispipo@gmail.com>
2023-09-09 06:39:55 +0100

    app.config.$common.json

--- src/realm/react/index.js
+++ src/realm/react/index.js
@@ -1,1 +1,7 @@
+import {createRealmContext} from '@realm/react';
 export * from '@realm/react';
+
+/*** permet de créer un contexte associé à l'objet Ream */
+export const createContext = (realmConfig)=>{
+    return createRealmContext(realmConfig);
+}

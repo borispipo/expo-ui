981928455b72639e916da156fe46c44713ee5e80
borispipo <borispipo@gmail.com>
2023-09-10 08:13:02 +0100

    Faire en sorte que le dossier expo-ui-root-path local soit sollicité uniquement en environnement web

--- expo-ui-path.js
+++ expo-ui-path.js
@@ -18,19 +18,21 @@ module.exports = function (){
         return path.resolve(__dirname,suffix).replace(sep,(sep+sep));///pour la résolution du module expo-ui en mode test
     }
     const expoUIPath = path.resolve(process.cwd(),"node_modules","@fto-consult","expo-ui");
-    const rootPath = process.cwd();
-    const src = path.resolve(rootPath,"src");
-    try {
-        const envObj = require("./parse-env")();
-        const euPathm = typeof envObj.EXPO_UI_ROOT_PATH =="string" && envObj.EXPO_UI_ROOT_PATH && path.resolve(envObj.EXPO_UI_ROOT_PATH)||'';
-        const eu = euPathm && fs.existsSync(euPathm)? euPathm : null;
-        if(eu &&  fs.existsSync(path.resolve(eu,"src")) && fs.existsSync(path.resolve(eu,"webpack.config.js"))){
-            return path.resolve(eu,suffix).replace(sep,(sep+sep));
+    if(process?.env?.WEBPACK_SERVE){
+        const rootPath = process.cwd();
+        const src = path.resolve(rootPath,"src");
+        try {
+            const envObj = require("./parse-env")();
+            const euPathm = typeof envObj.EXPO_UI_ROOT_PATH =="string" && envObj.EXPO_UI_ROOT_PATH && path.resolve(envObj.EXPO_UI_ROOT_PATH)||'';
+            const eu = euPathm && fs.existsSync(euPathm)? euPathm : null;
+            if(eu &&  fs.existsSync(path.resolve(eu,"src")) && fs.existsSync(path.resolve(eu,"webpack.config.js"))){
+                return path.resolve(eu,suffix).replace(sep,(sep+sep));
+            }
+        } catch{}
+        const expoUi = path.resolve(rootPath,"expo-ui");
+        if(fs.existsSync(src) && fs.existsSync(expoUi) && fs.existsSync(path.resolve(expoUi,"webpack.config.js"))){
+            return path.resolve(expoUi,suffix).replace(sep,(sep+sep));
         }
-    } catch{}
-    const expoUi = path.resolve(rootPath,"expo-ui");
-    if(fs.existsSync(src) && fs.existsSync(expoUi) && fs.existsSync(path.resolve(expoUi,"webpack.config.js"))){
-        return path.resolve(expoUi,suffix).replace(sep,(sep+sep));
     }
     return suffix ? path.join(expoUIPath,suffix).replace(sep,"/"): expoUIPath;
 };

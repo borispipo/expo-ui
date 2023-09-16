0cb0386527f9bfa0fe532f721de36d711bc6c338
borispipo <borispipo@gmail.com>
2023-09-16 07:46:05 +0100

    Fix gros bug lié à la possibilité de grouper les données du datagrid, lorsque le datagrid ne contient aucune colonne de type nombre

--- src/components/Table/Row/SectionListHeaderRow.js
+++ src/components/Table/Row/SectionListHeaderRow.js
@@ -1,9 +1,14 @@
 import {useTable} from "../hooks";
-export default function SectionListHeaderRow ({cells,rowIndex,rowData,headerContent}){
+import React from "$react";
+export default function SectionListHeaderRow ({cells:cCells,rowIndex,rowData,headerContent}){
     const {visibleColsNames} = useTable();
-    if(!Array.isArray(cells) || !cells.length){
+    const cells = Array.isArray(cCells)? cCells : [];
+    if(!cells.length){
+        if(React.isValidElement(headerContent)){
+            return <td colSpan={visibleColsNames.length} style={{padding:"10px"}} className="datagrid-table-section-list-header-unique-cell" children={headerContent}/>
+        }
         return visibleColsNames.map((c,i)=>{
-            return <td key={i}/>
+            return <td key={i} className={"datagrid-table-section-list-header-no-cell"} style={{width:"0",height:0,display:"none",opacity:0}}/>
         })
     }
     return cells;

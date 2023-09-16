2fee681577100368e081e56a91a8123ad7e5f1fb
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2023-09-16 13:06:57 +0100

    Remner le storage à AsyncStorage

--- App.js
+++ App.js
@@ -1,1 +1,16 @@
-export {default} from "./src";
+import AppEntry from "./src";
+import appConfig from "$capp/config";
+
+export default function MainExpoApp(props){
+    return <AppEntry
+        navigation={{
+            screens : require("./src/test-screens").default
+        }}
+        init ={({appConfig})=>{
+            appConfig.set("isAuthSingleUserAllowed",true);
+            appConfig.set("authDefaultUser",{code:"root",password:"admin123",label:"Master admin"})
+            return Promise.resolve("test ted")
+        }}
+        {...props}
+    />
+}

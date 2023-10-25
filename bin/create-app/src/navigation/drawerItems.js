d74344ef8e8e9b721549c9acc7bf35890f462b80
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2023-10-25 11:47:52 +0100

    Update et commentaire sur les drawerItems

--- bin/create-app/src/navigation/drawerItems.js
+++ bin/create-app/src/navigation/drawerItems.js
@@ -1,4 +1,14 @@
 
+/*** here you can export list of main drawerItems,
+    [key1] : {
+        label{string|ReactNode} : drawerLabel1,
+        routeName {string}, drawer routeName
+        routeParams {object}, list of custom params to pass to the navignation route,
+        icon {string|ReactNode}, icon,
+        left {function|string|ReactNode}, drawerItemLeft
+        right {function|string|ReactNode}, drawerItemRight,
+    }
+*/
 export default {
     
 }

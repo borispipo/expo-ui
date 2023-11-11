2e75e85f895e3937a38599d11163bd53ecd080f3
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2023-11-11 07:38:37 +0100

    Update de la fonction copy-file-env permettant de copier le fichier de variable d'environnement, la variable d'environnement sera dorénavant prise en compte en tenant compte de l'environnemnet actuel

--- copy-env-file.js
+++ copy-env-file.js
@@ -6,10 +6,12 @@ const writeFile = require("./electron/utils/writeFile");
  * 
  */
 module.exports = (projectRoot,forceCreate)=>{
-    const isDevEnv = 'development' === process.env.NODE_ENV;
+    const env = typeof process.env.NODE_ENV =="string" && process.env.NODE_ENV.toLowerCase().trim() || "development";
+    const isDevEnv = 'development' === env;
     if(!isDevEnv && process.env.IGNORE_ENV) return null;
     projectRoot = projectRoot && typeof projectRoot =="string" && fs.existsSync(projectRoot) && projectRoot || process.cwd();
-    const environmentPath = path.resolve(projectRoot,".env");
+    const pWithEnv = path.resolve(projectRoot,`.env.${env}`);
+    const environmentPath = fs.existsSync(pWithEnv) ? pWithEnv : path.resolve(projectRoot,".env"); 
     const localEnv = path.resolve(__dirname,".env");
     if(environmentPath && fs.existsSync(environmentPath)){
         // File ".env" will be created or overwritten by default.

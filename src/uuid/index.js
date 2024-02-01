8d463de2ba2e1da60ded973cc6fd8f5e84a20130
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2024-02-01 22:02:49 +0100

    Update du package common pour fix bug lié à la resolution des bases de données Prise en compte du package uuid pour la gestion de l'uuid react native

---
+++ src/uuid/index.js
@@ -0,0 +1,4 @@
+import uuid from 'react-native-uuid';
+const {parse,unparse,validate,version,v1,v3,v4,v5,NIL,DNS,URL,OID,X500,} = uuid;
+export {parse,unparse,validate,version,v1,v3,v4,v5,NIL,DNS,URL,OID,X500}
+export default uuid;

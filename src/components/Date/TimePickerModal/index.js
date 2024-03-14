2a7053c867a93cbb5ee7a393c606c71d4781b580
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2024-03-14 05:28:44 +0100

    Fix bug sur le composant Time qui n'affiche pas convenablement les icones

--- src/components/Date/TimePickerModal/index.js
+++ src/components/Date/TimePickerModal/index.js
@@ -1,9 +1,16 @@
 import {TimePickerModal} from 'react-native-paper-dates';
 import {PaperProvider} from 'react-native-paper';
 import theme from "$theme";
+import FontIcon from "$ecomponents/Icon/Font";
 
 export default function TimePickerModalComponent(props){
-    return <PaperProvider theme={theme}>
+    return <PaperProvider theme={theme}
+        settings={{
+            icon: (props) => {
+              return <FontIcon {...props}/>
+            },
+        }}
+    >
         <TimePickerModal {...props}/>
     </PaperProvider>
 }

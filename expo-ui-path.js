eefef269946856ad4a794b49a0b2ad48aa965861
borispipo <borispipo@gmail.com>
2024-05-08 17:42:16 +0100

    Fix bug : D:\PROJETS\VSCODE\new-dash\node_modules\@fto-consult\expo-ui\fto-consult-path.js:6:12)     at module.exports (D:\PROJETS\VSCODE\new-dash\node_modules\@fto-consult\expo-ui\fto-consult-path.js:6:41)

--- expo-ui-path.js
+++ expo-ui-path.js
@@ -1,32 +1,3 @@
-// Copyright 2022 @fto-consult/Boris Fouomene. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-const fs = require("fs");
-const path = require("path");
-const {JSONManager} = require("@fto-consult/node-utils");
-///retourne le chemin vers le package @expo-ui
 module.exports = function (projectRoot){
-    const sep = path.sep;
-    if(require("./is-local-dev")()){//le programme s'exécute en environnement fix bugs sur electron
-        return path.resolve(__dirname).replace(sep,(sep+sep));///pour la résolution du module expo-ui en mode test
-    }
-    const isDev = String(process.env.NODE_ENV).toLowerCase().trim() !="production";
-    projectRoot = typeof projectRoot =='string' && fs.existsSync(path.resolve(projectRoot)) && path.resolve(projectRoot) || process.cwd();
-    const packageJSON = path.resolve(projectRoot,"package.json");
-    const expoUIPath = path.resolve(projectRoot,"node_modules","@fto-consult","expo-ui");
-    if(isDev && fs.existsSync(packageJSON)){
-        const pM = JSONManager(packageJSON);
-        if(pM.hasPackage){
-            const expoUIRootPath = pM.get("expoUIRootPath");
-            if(typeof expoUIRootPath =="string" && expoUIRootPath){
-                const p = path.resolve(expoUIRootPath);
-                const pM2 = JSONManager(path.resolve(expoUIRootPath,"package.json"));
-                if(pM2.hasPackage && pM2.get("name") =="@fto-consult/expo-ui" && fs.existsSync(p) && fs.existsSync(path.resolve(p,"src"))){
-                    return p;
-                }
-            }
-        }
-    }
-    return expoUIPath;
+    return require("./fto-consult-path")(projectRoot,"@fto-consult/expo-ui","expoUIRootPath");
 };
--- fto-consult-path.js
+++ fto-consult-path.js
@@ -1,7 +1,33 @@
 // Copyright 2022 @fto-consult/Boris Fouomene. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+const fs = require("fs");
+const path = require("path");
+const {JSONManager} = require("@fto-consult/node-utils");
 ///retourne le chemin vers le package @expo-ui
-module.exports = function (projectRoot){
-    return require("./fto-consult-path")(projectRoot,"@fto-consult/expo-ui","expoUIRootPath");
+/***
+ * @param {string} projectRoot, le project root
+ * @param {string} moduleName, le module name
+ * @param {string} packageKey, la clé dans le package
+ */
+module.exports = function (projectRoot,moduleName,packageKey){
+    const isDev = String(process.env.NODE_ENV).toLowerCase().trim() !="production";
+    projectRoot = typeof projectRoot =='string' && fs.existsSync(path.resolve(projectRoot)) && path.resolve(projectRoot) || process.cwd();
+    const packageJSON = path.resolve(projectRoot,"package.json");
+    const ftoConsultPath = path.resolve(projectRoot,"node_modules",moduleName);
+    if(isDev && fs.existsSync(packageJSON)){
+        const pM = JSONManager(packageJSON);
+        if(pM.hasPackage){
+            const moduleRoutePath = pM.get(packageKey);
+            if(typeof moduleRoutePath =="string" && moduleRoutePath){
+                const p = path.resolve(moduleRoutePath);
+                const pM2 = JSONManager(path.resolve(moduleRoutePath,"package.json"));
+                if(pM2.hasPackage && pM2.get("name") == moduleName && fs.existsSync(p)){
+                    return p;
+                }
+            }
+        }
+    }
+    return ftoConsultPath;
 };

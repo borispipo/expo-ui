87e6e2e0026d1657e3574dcb5dc2581e3ff03eb4
borispipo <borispipo@gmail.com>
2024-05-10 11:23:08 +0100

    Ajout du fichier openLibrairies à gitignore

--- src/screens/Help/openLibraries.js
+++ src/screens/Help/openLibraries.js
@@ -1,129 +1,211 @@
 module.exports = {
 	"@fto-consult/expo-ui": {
-		"name": "@fto-consult/expo-ui",
-		"version": "9.0.3",
-		"repository": {
-			"type": "git",
-			"url": "git+https://github.com/borispipo/expo-ui.git"
-		},
-		"homepage": "https://github.com/borispipo/expo-ui#readme"
+		"version": "9.2.1",
+		"url": "https://github.com/borispipo/expo-ui#readme",
+		"license": "ISC"
 	},
 	"@babel/plugin-proposal-export-namespace-from": {
 		"version": "7.18.9",
 		"url": "https://babel.dev/docs/en/next/babel-plugin-proposal-export-namespace-from",
 		"license": "MIT"
 	},
-	"@emotion/react": {
-		"version": "11.11.4",
-		"url": "https://github.com/emotion-js/emotion/tree/main/packages/react",
+	"@emotion/native": {
+		"version": "11.11.0",
+		"url": "https://emotion.sh",
+		"license": "MIT"
+	},
+	"@expo/html-elements": {
+		"version": "0.5.1",
+		"url": "https://github.com/expo/expo/tree/main/packages/html-elements",
 		"license": "MIT"
 	},
 	"@expo/metro-config": {
-		"version": "0.17.7",
+		"version": "0.18.3",
 		"url": "https://github.com/expo/expo.git",
 		"license": "MIT"
 	},
-	"@faker-js/faker": {
-		"version": "8.4.1",
-		"url": "https://github.com/faker-js/faker.git",
+	"@expo/metro-runtime": {
+		"version": "3.2.1",
+		"url": "https://github.com/expo/expo.git",
 		"license": "MIT"
 	},
-	"@fto-consult/common": {
-		"version": "4.53.0",
-		"url": "https://github.com/borispipo/common#readme",
+	"@expo/vector-icons": {
+		"version": "14.0.0",
+		"url": "https://expo.github.io/vector-icons",
+		"license": "MIT"
+	},
+	"@pchmn/expo-material3-theme": {
+		"version": "1.3.2",
+		"url": "https://github.com/pchmn/expo-material3-theme#readme",
+		"license": "MIT"
+	},
+	"@react-native-community/netinfo": {
+		"version": "11.3.1",
+		"url": "https://github.com/react-native-netinfo/react-native-netinfo#readme",
+		"license": "MIT"
+	},
+	"@react-native/assets-registry": {
+		"version": "0.72.0",
+		"url": "git@github.com:facebook/react-native.git",
+		"license": "MIT"
+	},
+	"@react-navigation/native": {
+		"version": "6.1.17",
+		"url": "https://reactnavigation.org",
+		"license": "MIT"
+	},
+	"@react-navigation/native-stack": {
+		"version": "6.9.26",
+		"url": "https://github.com/software-mansion/react-native-screens#readme",
+		"license": "MIT"
+	},
+	"@react-navigation/stack": {
+		"version": "6.3.29",
+		"url": "https://reactnavigation.org/docs/stack-navigator/",
+		"license": "MIT"
+	},
+	"@shopify/flash-list": {
+		"version": "1.6.4",
+		"url": "https://shopify.github.io/flash-list/",
+		"license": "MIT"
+	},
+	"babel-plugin-inline-dotenv": {
+		"version": "1.7.0",
+		"url": "https://github.com/brysgo/babel-plugin-inline-dotenv#readme",
 		"license": "ISC"
 	},
-	"apexcharts": {
-		"version": "3.49.0",
-		"url": "https://apexcharts.com",
-		"license": "MIT"
-	},
 	"babel-plugin-module-resolver": {
-		"version": "5.0.2",
+		"version": "5.0.0",
 		"url": "https://github.com/tleunen/babel-plugin-module-resolver.git",
 		"license": "MIT"
 	},
-	"file-saver": {
-		"version": "2.0.5",
-		"url": "https://github.com/eligrey/FileSaver.js#readme",
+	"expo": {
+		"version": "51.0.0",
+		"url": "https://github.com/expo/expo/tree/main/packages/expo",
 		"license": "MIT"
 	},
-	"google-libphonenumber": {
-		"version": "3.2.34",
-		"url": "https://ruimarinho.github.io/google-libphonenumber/",
-		"license": "(MIT AND Apache-2.0)"
-	},
-	"html2canvas": {
-		"version": "1.4.1",
-		"url": "https://html2canvas.hertzen.com",
+	"expo-camera": {
+		"version": "15.0.4",
+		"url": "https://docs.expo.dev/versions/latest/sdk/camera/",
 		"license": "MIT"
 	},
-	"htmlparser2-without-node-native": {
-		"version": "3.9.2",
-		"url": "git://github.com/fb55/htmlparser2.git",
+	"expo-clipboard": {
+		"version": "6.0.3",
+		"url": "https://docs.expo.dev/versions/latest/sdk/clipboard",
 		"license": "MIT"
 	},
-	"is-plain-obj": {
-		"version": "4.1.0",
+	"expo-document-picker": {
+		"version": "12.0.1",
+		"url": "https://docs.expo.dev/versions/latest/sdk/document-picker/",
 		"license": "MIT"
 	},
-	"jsbarcode": {
-		"version": "3.11.6",
-		"url": "https://github.com/lindell/JsBarcode#readme",
+	"expo-font": {
+		"version": "12.0.4",
+		"url": "https://docs.expo.dev/versions/latest/sdk/font/",
 		"license": "MIT"
 	},
-	"prop-types": {
-		"version": "15.8.1",
-		"url": "https://facebook.github.io/react/",
+	"expo-image-manipulator": {
+		"version": "12.0.3",
+		"url": "https://docs.expo.dev/versions/latest/sdk/imagemanipulator/",
 		"license": "MIT"
 	},
-	"react-content-loader": {
-		"version": "6.2.1",
-		"url": "https://github.com/danilowoz/react-content-loader",
+	"expo-image-picker": {
+		"version": "15.0.4",
+		"url": "https://docs.expo.dev/versions/latest/sdk/imagepicker/",
 		"license": "MIT"
 	},
-	"react-native-big-list": {
-		"version": "1.6.1",
-		"url": "https://marcocesarato.github.io/react-native-big-list-docs/",
-		"license": "GPL-3.0-or-later"
-	},
-	"react-native-iphone-x-helper": {
-		"version": "1.3.1",
-		"url": "https://github.com/ptelad/react-native-iphone-x-helper#readme",
+	"expo-intent-launcher": {
+		"version": "11.0.1",
+		"url": "https://docs.expo.dev/versions/latest/sdk/intent-launcher/",
 		"license": "MIT"
 	},
-	"react-native-mime-types": {
-		"version": "2.5.0",
+	"expo-linking": {
+		"version": "6.3.1",
+		"url": "https://docs.expo.dev/versions/latest/sdk/linking",
 		"license": "MIT"
 	},
-	"react-native-paper": {
-		"version": "5.12.3",
-		"url": "https://callstack.github.io/react-native-paper",
+	"expo-sharing": {
+		"version": "12.0.1",
+		"url": "https://docs.expo.dev/versions/latest/sdk/sharing/",
 		"license": "MIT"
 	},
-	"react-native-paper-dates": {
-		"version": "0.22.7",
-		"url": "https://github.com/web-ridge/react-native-paper-dates#readme",
+	"expo-sqlite": {
+		"version": "14.0.3",
+		"url": "https://docs.expo.dev/versions/latest/sdk/sqlite/",
 		"license": "MIT"
 	},
-	"sanitize-filename": {
-		"version": "1.6.3",
-		"url": "git@github.com:parshap/node-sanitize-filename.git",
-		"license": "WTFPL OR ISC"
+	"expo-status-bar": {
+		"version": "1.12.1",
+		"url": "https://docs.expo.dev/versions/latest/sdk/status-bar/",
+		"license": "MIT"
+	},
+	"expo-system-ui": {
+		"version": "3.0.4",
+		"url": "https://docs.expo.dev/versions/latest/sdk/system-ui",
+		"license": "MIT"
+	},
+	"expo-web-browser": {
+		"version": "13.0.3",
+		"url": "https://docs.expo.dev/versions/latest/sdk/webbrowser/",
+		"license": "MIT"
+	},
+	"react-native": {
+		"version": "0.74.1",
+		"url": "https://reactnative.dev/",
+		"license": "MIT"
+	},
+	"react-native-gesture-handler": {
+		"version": "2.16.2",
+		"url": "https://github.com/software-mansion/react-native-gesture-handler#readme",
+		"license": "MIT"
+	},
+	"react-native-get-random-values": {
+		"version": "1.11.0",
+		"license": "MIT"
+	},
+	"react-native-reanimated": {
+		"version": "3.10.0",
+		"url": "https://github.com/software-mansion/react-native-reanimated#readme",
+		"license": "MIT"
+	},
+	"react-native-safe-area-context": {
+		"version": "4.10.1",
+		"url": "https://github.com/th3rdwave/react-native-safe-area-context#readme",
+		"license": "MIT"
+	},
+	"react-native-screens": {
+		"version": "3.31.1",
+		"url": "https://github.com/software-mansion/react-native-screens#readme",
+		"license": "MIT"
+	},
+	"react-native-svg": {
+		"version": "15.2.0-rc.0",
+		"url": "https://github.com/react-native-community/react-native-svg",
+		"license": "MIT"
+	},
+	"react-native-view-shot": {
+		"version": "3.8.0",
+		"url": "https://github.com/gre/react-native-view-shot",
+		"license": "MIT"
+	},
+	"react-native-web": {
+		"version": "0.19.11",
+		"url": "git://github.com/necolas/react-native-web.git",
+		"license": "MIT"
+	},
+	"react-native-webview": {
+		"version": "13.8.6",
+		"url": "https://github.com/react-native-webview/react-native-webview#readme",
+		"license": "MIT"
+	},
+	"react-virtuoso": {
+		"version": "4.7.10",
+		"url": "https://virtuoso.dev/",
+		"license": "MIT"
 	},
 	"sharp-cli": {
 		"version": "2.1.1",
 		"url": "https://github.com/vseventer/sharp-cli",
 		"license": "MIT"
-	},
-	"tippy.js": {
-		"version": "6.3.7",
-		"url": "https://atomiks.github.io/tippyjs/",
-		"license": "MIT"
-	},
-	"xlsx": {
-		"version": "0.18.5",
-		"url": "https://sheetjs.com/",
-		"license": "Apache-2.0"
 	}
 };
